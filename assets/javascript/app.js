5de34283ff8b1c2c3fd87f020eabf064f554fd2c
AR123456 <coloradoz99@gmail.com>
2018-06-26 19:42:01 -0600

    linking and updates to JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,14 @@
-alert("linked");
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAAhTjtnp-UvBAum4mEzgu7gI0Ayg8KMac",
+  authDomain: "time-sheet-be050.firebaseapp.com",
+  databaseURL: "https://time-sheet-be050.firebaseio.com",
+  projectId: "time-sheet-be050",
+  storageBucket: "time-sheet-be050.appspot.com",
+  messagingSenderId: "459994441579"
+};
+firebase.initializeApp(config);
+
+$("#add-employee-btn").on("click", function() {
+  alert("been clicked ");
+});
