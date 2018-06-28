3f9aa48a6870928a05e4c090aa1e1e66c4e8d607
AR123456 <coloradoz99@gmail.com>
2018-06-27 19:01:41 -0600

    updates to App logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,32 +9,68 @@ var config = {
 };
 firebase.initializeApp(config);
 const database = firebase.database();
-let name = "";
-let role = "";
-let start = "";
-let rate = 0;
+
+// let name = "";
+// let role = "";
+// let start = "";
+// let rate = 0;
 
 $("#add-employee-btn").on("click", function() {
   event.preventDefault();
 
-  name = $("#name-input")
+  let name = $("#name-input")
     .val()
     .trim();
-  role = $("#role-input")
+  let role = $("#role-input")
     .val()
     .trim();
-  start = $("#start-input")
-    .val()
-    .trim();
-  rate = $("#rate-input")
+  let start = moment(
+    $("#start-input")
+      .val()
+      .trim(),
+    "DD/MM/YY"
+  ).format("X");
+  let rate = $("#rate-input")
     .val()
     .trim();
 
-  database.ref().push({
+  const newEmp = {
     name: name,
     role: role,
     start: start,
     rate: rate,
     dateAdded: firebase.database.ServerValue.TIMESTAMP
-  });
+  };
+  database.ref().push(newEmp);
+  $("#name-input").val("");
+  $("#role-input").val("");
+  $("#start-input").val("");
+  $("#rate-input").val("");
+});
+
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+  let name = childSnapshot.val().name;
+  let role = childSnapshot.val().role;
+  let start = childSnapshot.val().start;
+  let rate = childSnapshot.val().rate;
+
+  let startPretty = moment.unix(start).format("MM/DD/YY");
+  let empMonths = moment().diff(moment.unix(start, "X"), "months");
+  let empBilled = empMonths * rate;
+
+  $("#employee-table > tbody").append(
+    "<tr><td>" +
+      name +
+      "</td><td>" +
+      role +
+      "</td><td>" +
+      startPretty +
+      "</td><td>" +
+      empMonths +
+      "</td><td>" +
+      rate +
+      "</td><td>" +
+      empBilled +
+      "</td></tr>"
+  );
 });
